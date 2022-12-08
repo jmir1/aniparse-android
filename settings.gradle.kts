89c5dd0b394f830ce24f883aff0e993aa576d2f2
GitHub Action <action@github.com>
2022-12-08 09:10:16 +0000

    Template cleanup

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -13,7 +13,7 @@ dependencyResolutionManagement {
     }
 }
 
-rootProject.name = ("kotlin-android-template")
+rootProject.name = ("aniparseandroid")
 
 enableFeaturePreview("TYPESAFE_PROJECT_ACCESSORS")
 
